da755f9d27f0382524e177082fafc8f03719dfaf
ting436 <tinghui1804@gmail.com>
2026-08-20 23:02:23 +0800

    Update the code to echo commands entered by the user, and exit when the user types the command bye

--- src/main/java/Unicorn.java
+++ src/main/java/Unicorn.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Unicorn {
     public static void main(String[] args) {
         String banner = "          /\\\n"
@@ -12,9 +14,18 @@ public class Unicorn {
                 + "     /_|___|_\\\n"
                 + "       /   \\\n"
                 + "      ✨   ✨\n";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(banner);
         System.out.println("Hello! I'm Unicorn.");
         System.out.println("What can I do for you?");
+        String input = scanner.nextLine();
+
+        while (!Objects.equals(input, "bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
