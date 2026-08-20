8bf209ef45d9280184cb09c0d315082032db4368
ting436 <tinghui1804@gmail.com>
2026-08-20 22:54:43 +0800

    Rename chatbot and add greeting and exit messages

---
+++ src/main/java/Unicorn.java
@@ -0,0 +1,20 @@
+public class Unicorn {
+    public static void main(String[] args) {
+        String banner = "          /\\\n"
+                + "         /  \\\n"
+                + "        / /\\ \\\n"
+                + "       / /  \\ \\\n"
+                + "      /_/    \\_\\\n"
+                + "     (  ^  ^  )\n"
+                + "      \\  ♥  /\n"
+                + "       \\___/\n"
+                + "      /|   |\\\n"
+                + "     /_|___|_\\\n"
+                + "       /   \\\n"
+                + "      ✨   ✨\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Unicorn.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
