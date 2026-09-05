09ba5e5c15e9846370a9710fcf6b2b0c5923632a
ting436 <tinghui1804@gmail.com>
2026-09-05 18:27:40 +0800

    Design the layout

--- src/main/java/unicorn/Main.java
+++ src/main/java/unicorn/Main.java
@@ -1,5 +1,6 @@
 package unicorn;
 
+import javafx.scene.layout.Region;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -45,5 +46,34 @@ public class Main extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        stage.setTitle("Unicorn");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
     }
 }
