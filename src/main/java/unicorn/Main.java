187299dea52f100c4fdd4743c272e8ee1219f262
ting436 <tinghui1804@gmail.com>
2026-09-05 18:24:05 +0800

    Add user image

--- src/main/java/unicorn/Main.java
+++ src/main/java/unicorn/Main.java
@@ -3,17 +3,45 @@ package unicorn;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import unicorn.ui.DialogBox;
 
 /**
  * Displays the Unicorn JavaFX user interface.
  */
 public class Main extends Application {
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image unicornImage = new Image(this.getClass().getResourceAsStream("/images/Unicorn.png"));
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
 
         stage.setScene(scene);
         stage.show();
